1899288f979c67852180374354b9979de72ebdf3
san03851 <sangcheul.na@gmail.com>
2026-02-25 09:18:22 +0900

    std out 으로 고침
    printf -> std out

--- calculator.cpp
+++ calculator.cpp
@@ -44,33 +44,39 @@ int main()
 	std::cin >> First;
 	std::cin >> Second;
 	printf("First number : %d \n Second number : %d \n", First, Second);
-
+	std::cout << "First number : "  << First << "\n";
+	std::cout << "Second number : " << Second << "\n";
 	for (int i = 0; i < 5; i++)
 	{
 		if (i == 0)
 		{
 			Result = Sum(First, Second);
-			printf("Sum result : %d \n", Result);
+			//printf("Sum result : %d \n", Result);
+			std::cout << "Sum result : " << Result << "\n";
 		}
 		else if (i == 1)
 		{
 			Subtract(First, Second);
-			printf("Subtraction result : %d \n", Result);
+			//printf("Subtraction result : %d \n", Result);
+			std::cout << "Subtraction result : " << Result << "\n";
 		}
 		else if (i == 2)
 		{
 			Divide(First, Second);
-			printf("Division result : %d \n", Result);
+			//printf("Division result : %d \n", Result);
+			std::cout << "Division result : " << Result << "\n";
 		}
 		else if (i == 3)
 		{
 			Multiply(First, Second);
-			printf("Multiplication result : %d \n", Result);
+			//printf("Multiplication result : %d \n", Result);
+			std::cout << "Multiplication result : " << Result << "\n";
 		}
 		else if (i == 4)
 		{
 			Modulo(First, Second);
-			printf("Modulo result : %d \n", Result);
+			//printf("Modulo result : %d \n", Result);
+			std::cout << "Modulo result : " << Result << "\n";
 		}
 
 	}
